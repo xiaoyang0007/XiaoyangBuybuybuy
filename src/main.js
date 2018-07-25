268c1df42ebe204c13526b2aab255c6cb5244c04
xiaoyang <787442896@qq.com>
2018-07-25 20:49:47 +0800

    完成首页

--- src/main.js
+++ src/main.js
@@ -1,8 +1,32 @@
 import Vue from 'vue'
 import App from './App.vue'
+// 引入路由模板
+import VueRouter from 'vue-router'
+// 引入index组件
+import index from './components/index.vue'
+// 使用路由中间件
+Vue.use(VueRouter)
+
+// 注册路由规则
+const router = new VueRouter({
+        routes: [{
+                path: '/',
+                component: index
+            },
+            {
+                path: '/index',
+                component: index
+            }
+        ]
+    })
+    // 引入css
+import './assets/statics/site/css/style.css'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App)
-}).$mount('#app')
+    el: '#app',
+    // 挂载vue
+    router,
+    render: h => h(App)
+})
